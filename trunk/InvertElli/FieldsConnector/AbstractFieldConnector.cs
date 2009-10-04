aa53ffcad9a2e3b98ca6b121032e3c2e5b276f0e
nettrots <nettrots@f5468948-9d8d-11de-b798-49b661d4457b>
2009-10-04 21:48:07 +0000

    FieldConnector abstractions and realization

---
+++ trunk/InvertElli/FieldsConnector/AbstractFieldConnector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FieldsConnector
+{
+    public abstract class AbstractFieldConnector
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        protected abstract void syncTo();
+        protected abstract void syncFrom();
+
+
+        
+    }
+ 
+   
+
+}
+/*
+ connect(Form.Textbox.Text, Instance.Property,name(string) )
+ FormToSearchCnn[name].syncTo()
+ FormToSearchCnn[name].syncFrom()
+ */
---
+++ trunk/InvertElli/FieldsConnector/FieldsList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FieldsConnector
+{
+    public class FieldsList
+    {
+
+        private Dictionary<string, AbstractFieldConnector> _list;
+        public AbstractFieldConnector this[string index]
+        {
+
+            get
+            {
+                return _list[index];
+
+            }
+
+        }
+        public void addTier(AbstractFieldConnector afc)
+        {
+            _list.Add(afc.Name, afc);
+        }
+
+    }
+}
